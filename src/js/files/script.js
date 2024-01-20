2186a86d39966fc46b5d59eecd9cf7d0f54a2e47
Wlad199 <habilis199@mail.ru>
2024-01-20 11:53:14 +0300

    select

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,3 +8,59 @@ import { flsModules } from "./modules.js";
 //$(document).ready(function () {
 //	$('.comment__body').slick();
 //})
+
+// marks car =================//
+
+let carList = document.querySelector('.marks-select')
+
+carList.addEventListener('click', function (e) {
+
+	let checkItem = e.target.closest('.marks-select__item');
+	if (checkItem) {
+		checkItem.classList.toggle('_cheched')
+	}
+})
+
+
+
+
+// noUiSlider ====================================================================================================//
+
+var snapSlider = document.getElementById('range__snap');
+
+noUiSlider.create(snapSlider, {
+	start: [0, 1100],
+	snap: true,
+	//tooltips: true,
+	connect: true,
+	range: {
+		'min': 0,
+		'10%': 500,
+		'20%': 800,
+		'30%': 1100,
+		'40%': 1400,
+		'50%': 1700,
+		'60%': 2000,
+		'70%': 2300,
+		'80%': 2700,
+		'90%': 3000,
+		'max': 3500
+	},
+	pips: {
+		mode: 'range',
+		density: 10
+	}
+});
+
+let marginMin = document.querySelector('.marginMin')
+let marginMax = document.querySelector('.marginMax')
+
+snapSlider.noUiSlider.on('update', (values, handle) => {
+	if (handle) {
+		marginMax.innerHTML = +values[handle];
+	} else {
+		marginMin.innerHTML = +values[handle];
+	}
+})
+
+//  ====================================================================================================//
--- src/js/files/slickSlider.js
+++ src/js/files/slickSlider.js
@@ -26,23 +26,23 @@ $(document).ready(function () {
 // New Slider ====================================================================================================//
 
 $(document).ready(function () {
-	$('.slider').slick({
-		//arrows: true, // стрелки
-		//dots: true, // точки
-		//slidesToShow: 3, // кол-во показ. слайдов
-		//slidesToScroll: 1, // кол-во прокручиваемых слайдов за раз
-		//speed: 1000, // скорость (400)
+	$('.main-screen__box').slick({
+		arrows: true, // стрелки
+		dots: true, // точки
+		slidesToShow: 1, // кол-во показ. слайдов
+		slidesToScroll: 1, // кол-во прокручиваемых слайдов за раз
+		speed: 500, // скорость (400)
 		//autoplay: false, // автовоспроизведение (false)
 		//autoplaySpeed: 1000, // скорость (3000)
 
-		//responsive:[
-		//	{
-		//		breakpoint: 768,
-		//		settings: {
-		//			менять свойства при брейкпоинте
-		//		}
-		//	}
-		//]
+		responsive: [
+			{
+				breakpoint: 400,
+				settings: {
+					arrows: false,
+				}
+			}
+		]
 
 	});
 });
